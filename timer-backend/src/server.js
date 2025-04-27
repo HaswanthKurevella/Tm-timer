c250119affba8e9312c992b400daf3e297772641
haswanth <haswanthkurevella1@gmail.com>
2025-04-27 15:41:10 +0530

    orgin updated

--- timer-backend/src/server.js
+++ timer-backend/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://tm-timer-8jrz.vercel.app/"],
+    origin: ["https://toastmasters-time-keeper.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
